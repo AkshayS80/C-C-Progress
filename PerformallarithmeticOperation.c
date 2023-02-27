5d7c56d612bee2a58b432d9b6cf4b2f8e40a67f0
AkshayS80 <107939138+AkshayS80@users.noreply.github.com>
2023-02-27 19:01:02 +0530

    Create PerformallarithmeticOperation.c

---
+++ PerformallarithmeticOperation.c
@@ -0,0 +1,18 @@
+#inlcude <stdio.h>
+int main()
+{
+  float a,b,c,d,e,f;
+  printf("Enter first number:");
+  scanf("%f",&a);
+  printf("Enter second number:");
+  scanf("%f",&b);
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  printf("addition= %f",c);
+  printf("/nsubtraction= %f",d);
+  printf("/nmultiplication= %f",e);
+  printf("/ndivision= %f",f);
+  return 0;
+}
