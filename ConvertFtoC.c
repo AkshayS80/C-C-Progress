cbdf9649e9a48dc84cd36eb4894ee0ff722bb3aa
AkshayS80 <107939138+AkshayS80@users.noreply.github.com>
2023-02-27 18:53:00 +0530

    Create ConvertFtoC.c

---
+++ ConvertFtoC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  float f,c;
+  printf("Enter the Farenheit value:");
+  scanf("%f",&f);
+  c=(32-f)*5/9;
+  printf("Hence, the celsius value of %f",f);
+  printf("\ is %f",c);
+  return 0;
+}
