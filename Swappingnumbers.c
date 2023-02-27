e61ecb945ccadbf28baba1d1584068e11cfd0093
AkshayS80 <107939138+AkshayS80@users.noreply.github.com>
2023-02-27 19:04:22 +0530

    Create Swappingnumbers.c

---
+++ Swappingnumbers.c
@@ -0,0 +1,19 @@
+#inlcude <stdio.h>
+int main()
+{
+  float a,b,c;
+  printf("Enter first number:");
+  scanf("%f",&a);
+  printf("Enter second number:");
+  scanf("%f",&b);
+  printf("Before Swapping, The numbers are:")
+  printf("\ %f",a);
+  printf("\ %f",b);
+  c=a;
+  a=b;
+  b=c;
+  printf("\nAfter Swapping, The numbers are:")
+  printf("\ %f",a);
+  printf("\ %f",b);
+  return 0;
+}
