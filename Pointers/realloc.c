424f09f126ba234164524dc37dc59e9ff8fda9d3
Akshay Sharma <107939138+AkshayS80@users.noreply.github.com>
2023-05-13 19:10:42 +0530

    Update realloc.c

--- Pointers/realloc.c
+++ Pointers/realloc.c
@@ -5,32 +5,35 @@ int main(){
 	int *ptr, n,i;
 	printf("Enter the number of elements:");
 	scanf("%d",&n);
-	ptr=(int*)calloc(n,sizeof(int));
+	ptr=(int*)malloc(n*sizeof(int));
 	if (ptr==NULL){
 		printf("Memory not allocated\n");
 	}
 	else{
-		printf("Memory successfully allocated using calloc\n");
-		printf("Enter the elements;");
+		printf("Memory successfully allocated using malloc\n");
+		printf("Enter the elements");
 		for(i=0;i<n;++i){
-			scanf("%d\n",&ptr[i]);
+			scanf("%d",&ptr[i]);
 		}	
-		printf("The elements are");
-		for(i=0;i<n;++i){
-			printf("%d\n",ptr[i]);
-		}
-		printf("Enter the new size of the array:");
-		scanf("%d",&n);
-		ptr=realloc(ptr,n*sizeof(int));
-		printf("Memory successfully reallocated using realloc\n");
-		printf("Enter the elements;");
-		for(i=0;i<n;++i){
-			scanf("%d\n",&ptr[i]);
-		}	
-		printf("The elements are");
+		printf("The elements are\n");
 		for(i=0;i<n;++i){
 			printf("%d\n",ptr[i]);
 		}	
+		printf("Enter the number of elements of the new array:");
+	    scanf("%d",&n);
+		ptr = realloc(ptr, n * sizeof(int));
+        printf("Memory successfully re-allocated using realloc.\n");
+ 
+        
+        printf("Enter the elements of the new array:\n");
+        for(i=0;i<n;++i){
+			scanf("%d",&ptr[i]);
+        }
+        // Print the elements of the array
+        printf("The elements of the array are:\n ");
+        for (i = 0; i < n; ++i) {
+            printf("%d, ", ptr[i]);
+        }
 	}
 	return 0;
 }
